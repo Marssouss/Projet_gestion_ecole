5b7666e0d1df176957e885a37ad89ac36b3a4e4e
Olariia <michele.raffaelli@orange.fr>
2020-01-31 15:07:34 +0100

    Ajout entity Aide

---
+++ Gestion_Ecole/src/com/intiformation/gestionecole/entity/Aide.java
@@ -0,0 +1,44 @@
+package com.intiformation.gestionecole.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Aide implements Serializable {
+	/* Propriétés */
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="id_aide")
+	private Long idAide;
+	
+	@Column(name="page")
+	private int page;
+	
+	@Column(name="contenu")
+	private String contenu;
+
+	/* Constructeurs */
+	//Constructeur vide
+	protected Aide() {
+		
+	} //end ctor vide
+
+	//Constructeur chargé toutes infos
+	protected Aide(Long idAide, int page, String contenu) {
+		this.idAide = idAide;
+		this.page = page;
+		this.contenu = contenu;
+	} //end ctor chargé toutes infos
+
+	//Constructeur chargé sans ID
+	protected Aide(int page, String contenu) {
+		this.page = page;
+		this.contenu = contenu;
+	} //end ctor chargé sans ID
+
+} //end class
